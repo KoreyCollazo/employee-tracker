e326fab49c64773ad64614c0964c850e95325f7f
Korey Collazo <koreycollazo@Koreys-MacBook-Pro.local>
2022-12-19 17:16:32 -0500

    created schema for project

---
+++ schema/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (name)
+VALUES ("hr"),
+       ("lab"),
+       ("software");
+
+INSERT INTO role (title, salary)
+VALUES ("boss", 155.555),
+       ("not boss", 65.00),
+       ("miniboss", 2.00);
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES (1, "The Great Gatsby", "jill"),
+       (2, "Huckleberry Finn", "bob"),
+       (3, "100 Years of Solitude", "joe");
